ee923fc0a205cbe04f39a124c458c5f4e001c7bf
Muhamed Curri <muhamedcurri1@gmail.com>
2023-12-14 15:06:50 +0300

    add kelimeler and progression bar and made a card

--- lib/kategori_widget.dart
+++ lib/kategori_widget.dart
@@ -7,11 +7,46 @@ class KategoriWidget extends StatelessWidget{
   Widget build(BuildContext context){
     return Column(
       children: [
-        Expanded(child: ListView.builder(
-          itemCount: liste.length,
-          itemBuilder: (context, index) => ListTile(
-            title: Text(liste[index].isim),
-            leading: Image.asset('assets/${liste[index].isim}.png')
+        Expanded(child: Center(
+          child: ListView.builder(
+            itemCount: liste.length,
+            itemBuilder: (context, index) => SizedBox(
+              height: 120,
+              child: Card(
+                margin: EdgeInsets.all(10),
+                color: const Color.fromARGB(255, 189, 226, 243),
+                elevation: 2,
+                child: ListTile(
+                  title: Column(
+                    
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children:[
+                     Text(liste[index].isim),
+                     Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            Text("${liste[index].ogrenilenKelime}/${liste[index].toplamKelime}"),
+                            Text('Kelimeler')
+                          ],
+                        ),
+                      )),
+                     Container(
+                        margin: EdgeInsets.all(3),
+                        height: 10,
+                       child: LinearProgressIndicator(
+                        value: liste[index].progress,
+                        backgroundColor: Colors.grey,
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
+                       ),
+                     )
+                     ]),
+                  leading: Image.asset('assets/${liste[index].isim.toLowerCase()}.png')
+                ),
+              ),
+            ),
           ),
         ))
       ],
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:ing_uygulama/kategori.dart';
 import 'package:ing_uygulama/kategori_widget.dart';
@@ -5,12 +7,12 @@ import 'package:ing_uygulama/kategori_widget.dart';
 void main() {
   runApp(const MainApp());
 }
-var hayvanlar = Kategori("hayvanlar",0,0,0);
-  var meslekler = Kategori("meslekler",0,0,0);
-  var araclar =  Kategori("araclar",0,0,0);
-  var sporlar = Kategori("sporlar",0,0,0);
-  var meyve = Kategori("meyve",0,0,0);
-  var esya = Kategori("esya",0,0,0);
+var hayvanlar = Kategori("HAYVANLAR",0.3,3,10);
+  var meslekler = Kategori("MESLEKLER",0.9,9,10);
+  var araclar =  Kategori("ARACLAR",0.8,8,10);
+  var sporlar = Kategori("SPORLAR",0.4,4,10);
+  var meyve = Kategori("MEYVE",0.6,6,10);
+  var esya = Kategori("ESYA",0.1,1,10);
   List<Kategori> liste = [hayvanlar,meslekler,araclar,sporlar,meyve,esya];
 
 class MainApp extends StatelessWidget {
@@ -25,7 +27,18 @@ class MainApp extends StatelessWidget {
           child: Text('IngApp'),
           ),
           ),
-        body: KategoriWidget(liste)
+        body: KategoriWidget(liste),
+        bottomNavigationBar: BottomNavigationBar(items: [
+           BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.rocket),
+              label: 'gozden gecirme'),
+              BottomNavigationBarItem(icon: Icon(Icons.settings),
+              label: 'settings'),
+        ]),
       ),
     );
   }
