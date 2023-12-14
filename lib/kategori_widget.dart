2b71d15de56e4cd63fa8b8dda5c08fc79b1d2d43
Muhamed Curri <muhamedcurri1@gmail.com>
2023-12-14 16:49:01 +0300

    wrapped kategori with elevatedbutton

--- lib/kategori_widget.dart
+++ lib/kategori_widget.dart
@@ -10,40 +10,49 @@ class KategoriWidget extends StatelessWidget{
         Expanded(child: Center(
           child: ListView.builder(
             itemCount: liste.length,
-            itemBuilder: (context, index) => SizedBox(
-              height: 120,
-              child: Card(
-                margin: EdgeInsets.all(10),
-                color: const Color.fromARGB(255, 189, 226, 243),
-                elevation: 2,
-                child: ListTile(
-                  title: Column(
-                    
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children:[
-                     Text(liste[index].isim),
-                     Card(
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          children: [
-                            Text("${liste[index].ogrenilenKelime}/${liste[index].toplamKelime}"),
-                            Text('Kelimeler')
-                          ],
-                        ),
-                      )),
-                     Container(
-                        margin: EdgeInsets.all(3),
-                        height: 10,
-                       child: LinearProgressIndicator(
-                        value: liste[index].progress,
-                        backgroundColor: Colors.grey,
-                        valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
-                       ),
-                     )
-                     ]),
-                  leading: Image.asset('assets/${liste[index].isim.toLowerCase()}.png')
+            itemBuilder: (context, index) => ElevatedButton(
+              onPressed: (){},
+              child: SizedBox(
+                height: 155,
+                child: Card(
+                  margin: EdgeInsets.all(10),
+                  color: const Color.fromARGB(255, 189, 226, 243),
+                  elevation: 2,
+                  child: ListTile(
+                    title: Container(
+                      margin: EdgeInsets.all(3),
+                      padding: EdgeInsets.all(10),
+                      child: Column( 
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children:[
+                         Text(liste[index].isim),
+                         Card(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              children: [
+                                Text("${liste[index].ogrenilenKelime}/${liste[index].toplamKelime}"),
+                                Text('Kelimeler')
+                              ],
+                            ),
+                          )),
+                         Container(
+                            margin: EdgeInsets.fromLTRB(0,8,0,8),
+                            height: 10,
+                           child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                             child: LinearProgressIndicator(
+                              value: liste[index].progress,
+                              backgroundColor: Colors.grey,
+                              valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
+                             ),
+                           ),
+                         )
+                         ]),
+                    ),
+                    leading: Image.asset('assets/${liste[index].isim.toLowerCase()}.png')
+                  ),
                 ),
               ),
             ),
