1b90d9e730611bb6f6dd0995c3e8468680a49824
emircankocatepe <149237450+emircankocatepe@users.noreply.github.com>
2023-12-30 20:45:13 +0300

    Add files via upload

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/emircan123kocatepegmail.com/Developer/flutter-2"
+export "FLUTTER_APPLICATION_PATH=/Users/emircan123kocatepegmail.com/Desktop/flutter/projemizz"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/emircan123kocatepegmail.com/Desktop/flutter/projemizz/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=0.1.0"
+export "FLUTTER_BUILD_NUMBER=0.1.0"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9hNzk0Y2YyNjgxYzZjOWZlN2IyNjBlMGU4NGRlOTYyOThkYzljMThiLw=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/emircan123kocatepegmail.com/Desktop/flutter/projemizz/.dart_tool/package_config.json"
